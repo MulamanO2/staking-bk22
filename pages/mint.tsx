c2553bcd7668023ce33d69318824c9a7ded178dc
MulamanO2 <95545958+MulamanO2@users.noreply.github.com>
2022-08-28 21:38:57 -0700

    Add files via upload

---
+++ pages/mint.tsx
@@ -0,0 +1,59 @@
+import { useAddress, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Mint: NextPage = () => {
+  const router = useRouter();
+  // Get the currently connected wallet's address
+  const address = useAddress();
+
+  // Function to connect to the user's Metamask wallet
+  const connectWithMetamask = useMetamask();
+
+  // Get the NFT Collection contract
+  const nftDropContract = useNFTDrop(
+    "0xF628278364CE3613674D6bf85F8580eEa3b1Cd8c"
+  );
+
+  async function claimNft() {
+    try {
+      const tx = await nftDropContract?.claim(1);
+      console.log(tx);
+      alert("NFT Claimed!");
+      router.push(`/stake`);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Mint A Bored Kitty NFT!</h1>
+
+      <p className={styles.explain}>
+        Here is where you will be able to <b>MINT</b> a <b>BORED KITTY NFT</b>
+      </p>
+      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
+
+      {!address ? (
+        <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          onClick={connectWithMetamask}
+        >
+          Connect Wallet
+        </button>
+      ) : (
+        <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          onClick={() => claimNft()}
+        >
+          Claim A Kitty
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Mint;
